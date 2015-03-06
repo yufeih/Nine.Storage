57afa4938ce18b2c06fd7666536ebc24a39a49ef
Yufei Huang <yufeih@live.com>
2015-03-06 19:19:11 +0800

    Add AssemblyInfo and nuspecs

---
+++ test/Portable/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Nine.Storage.Test")]
+[assembly: AssemblyDescription("A portable key value pair storage")]
+[assembly: AssemblyCompany("Studio Nine")]
+[assembly: AssemblyProduct("Nine.Storage.Test")]
+[assembly: AssemblyCopyright("Copyright © Studio Nine 2015")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
