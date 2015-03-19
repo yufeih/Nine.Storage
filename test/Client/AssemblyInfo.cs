e18560c87849349a2f303af3ba9846df5c329ee2
Yufei Huang <yufeih@microsoft.com>
2015-03-19 14:54:00 +0800

    Make version compatible with appveryor.yml

--- test/Client/AssemblyInfo.cs
+++ test/Client/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
