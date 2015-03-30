bd2fdb26a3b1cf612ce6fc63462ce3a8742f806b
Yufei Huang <yufeih@microsoft.com>
2015-03-30 16:04:36 +0800

    Add a sample inline data to AzureStoragePartitionKeyTest

--- test/Server/AzureStoragePartitionKeyTest.cs
+++ test/Server/AzureStoragePartitionKeyTest.cs
@@ -6,10 +6,12 @@
     public class AzureStoragePartitionKeyTest
     {
         [Theory]
-        public void batch_storage_key_to_partition_key(string key, int partitionCount, int partitionKeyLength)
+        [InlineData("abcdefg", 32, 4, 26)]
+        public void batch_storage_key_to_partition_key(string key, int partitionCount, int partitionKeyLength, int expectedPartition)
         {
             var partition = BatchedTableStorage<TestStorageObject>.GetPartitionKey(key, partitionCount, partitionKeyLength);
             Trace.WriteLine($"{ key } => { partition }");
+            Assert.Equal(expectedPartition, partition);
         }
     }
 }
