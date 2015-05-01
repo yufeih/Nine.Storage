79ccaa24e123d08856d6fe9b6feee3b78c7e48af
Yufei Huang <yufeih@microsoft.com>
2015-05-01 22:30:39 +0800

    Remove Put/getOrAdd because the implementation is error prone

--- src/Portable/StorageExtensions.cs
+++ src/Portable/StorageExtensions.cs
@@ -51,17 +51,6 @@
             return storage.Get<T>(StorageKey.Get(keyComponents));
         }
 
-        public async static Task<T> GetOrAdd<T>(this IStorage storage, Func<T> factory = null, params object[] keyComponents) where T : class, IKeyed, new()
-        {
-            var result = await storage.Get<T>(StorageKey.Get(keyComponents)).ConfigureAwait(false);
-            if (result == null)
-            {
-                result = factory != null ? factory() : new T();
-                if (result != null) await storage.Put(result);
-            }
-            return result;
-        }
-
         public static Task<IEnumerable<T>> List<T>(this IStorage storage, params object[] keyComponents) where T : class, IKeyed, new()
         {
             var prefix = StorageKey.Get(keyComponents);
@@ -92,28 +81,6 @@
         public static Task Delete<T>(this IStorage storage, T value) where T : class, IKeyed, new()
         {
             return storage.Delete<T>(value.GetKey());
-        }
-
-        public static async Task<bool> Put<T>(this IStorage storage, string key, Action<T> action, Func<T> factory = null) where T : class, IKeyed, new()
-        {
-            var existing = await storage.Get<T>(key).ConfigureAwait(false) ?? (factory != null ? factory() :  new T());
-            action(existing);
-            if (key != null && existing.GetKey() == null) throw new InvalidOperationException("Put with an invalid key");
-            await storage.Put(existing).ConfigureAwait(false);
-            return true;
-        }
-
-        public static async Task<bool> Put<T>(this IStorage storage, string key, Func<T, bool> predicate, Action<T> action, Func<T> factory = null) where T : class, IKeyed, new()
-        {
-            var existing = await storage.Get<T>(key).ConfigureAwait(false) ?? (factory != null ? factory() : new T());
-            if (predicate(existing))
-            {
-                action(existing);
-                if (key != null && existing.GetKey() == null) throw new InvalidOperationException("Put with an invalid key");
-                await storage.Put(existing).ConfigureAwait(false);
-                return true;
-            }
-            return false;
         }
 
         public static Task<bool> Patch<T>(this IStorage storage, string key, Action<T> action) where T : class, IKeyed, new()
--- test/Client/StorageTest.cs
+++ test/Client/StorageTest.cs
@@ -19,21 +19,6 @@
         }
 
         [Fact]
-        public async Task it_should_create_a_new_instance_on_put()
-        {
-            TestStorageObject a = null, b = null, c = null;
-
-            var storage = new MemoryStorage();
-            await storage.Put<TestStorageObject>("1", p => { a = p; p.Id = "1"; });
-            await storage.Put<TestStorageObject>("2", p => { b = p; p.Id = "2"; });
-            await storage.Put<TestStorageObject>("2", p => { c = p; });
-
-            Assert.NotNull(a);
-            Assert.NotEqual(a, b);
-            Assert.Equal(b, c);
-        }
-
-        [Fact]
         public async Task it_should_put_deleted_items_into_recycle_bin()
         {
             var recycleBin = new MemoryStorage<TestStorageObject>();
