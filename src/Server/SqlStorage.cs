e12b72396b1a4b92db67b4dad02a02e10209e902
Yufei Huang <yufeih@live.com>
2015-05-09 11:15:50 +0800

    Implement SqlStorage.Range and change to create a new connection for each request.

--- src/Server/SqlStorage.cs
+++ src/Server/SqlStorage.cs
@@ -9,20 +9,19 @@
     using System.Threading.Tasks;
     using Nine.Formatting;
 
-    public class SqlStorage<T> : IDisposable, IStorage<T> where T : class, IKeyed, new()
+    public class SqlStorage<T> : IStorage<T> where T : class, IKeyed, new()
     {
         private static readonly List<SqlColumn> columns = SqlColumn.FromType(typeof(T)).ToList();
 
         private readonly TextConverter converter;
-        private readonly SqlConnection connection;
+        private readonly string connectionString;
         private readonly string tableName;
 
         public SqlStorage(string connectionString, string tableName = null, bool autoSchema = false, TextConverter converter = null)
         {
             if (string.IsNullOrEmpty(connectionString)) throw new ArgumentNullException(nameof(connectionString));
 
-            this.connection = new SqlConnection(connectionString);
-            this.connection.Open();
+            this.connectionString = connectionString;
             this.tableName = tableName ?? typeof(T).Name;
             this.converter = converter;
 
@@ -37,29 +36,37 @@
 
         private DataTable CreateTableIfNotExist()
         {
-            using (var command = connection.CreateCommand())
+            using (var connection = new SqlConnection(connectionString))
             {
-                command.CommandText = $"select top 0 * from { tableName }";
-
-                try
+                connection.Open();
+                using (var command = connection.CreateCommand())
                 {
+                    command.CommandText = $"select top 0 * from { tableName }";
+
+                    try
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            return reader.GetSchemaTable();
+                        }
+                    }
+                    catch (SqlException e) when (e.ErrorCode == -2146232060) { } // 0x80131904
+                }
+            }
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    var columnText = $"{ string.Concat(columns.Select(c => $"{ c.Name } { c.ToDbTypeText() }, ")) }";
+                    var constraint = $"constraint [PK_{ tableName }] primary key ({ SqlColumn.KeyColumnName })";
+                    command.CommandText = $"create table { tableName } ({ columnText } { constraint })";
+
                     using (var reader = command.ExecuteReader())
                     {
                         return reader.GetSchemaTable();
                     }
-                }
-                catch (SqlException e) when (e.ErrorCode == -2146232060) { } // 0x80131904
-            }
-
-            using (var command = connection.CreateCommand())
-            {
-                var columnText = $"{ string.Concat(columns.Select(c => $"{ c.Name } { c.ToDbTypeText() }, ")) }";
-                var constraint = $"constraint [PK_{ tableName }] primary key ({ SqlColumn.KeyColumnName })";
-                command.CommandText = $"create table { tableName } ({ columnText } { constraint })";
-
-                using (var reader = command.ExecuteReader())
-                {
-                    return reader.GetSchemaTable();
                 }
             }
         }
@@ -79,170 +86,236 @@
 
         public async Task<T> Get(string key)
         {
-            using (var command = connection.CreateCommand())
+            using (var connection = new SqlConnection(connectionString))
             {
-                var sb = StringBuilderCache.Acquire(260);
-                sb.Append("select * from ");
-                sb.Append(tableName);
-                sb.Append(" where ");
-                sb.Append(SqlColumn.KeyColumnName);
-                sb.Append(" = @key");
-
-                command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
-                command.Parameters.AddWithValue("@key", key);
-
-                using (var reader = await command.ExecuteReaderAsync().ConfigureAwait(false))
+                connection.Open();
+                using (var command = connection.CreateCommand())
                 {
-                    if (!reader.Read()) return null;
+                    var sb = StringBuilderCache.Acquire(260);
+                    sb.Append("select * from ");
+                    sb.Append(tableName);
+                    sb.Append(" where ");
+                    sb.Append(SqlColumn.KeyColumnName);
+                    sb.Append(" = @key");
 
-                    var result = new T();
-                    for (int i = 1; i < columns.Count; i++) // Skip the first '_Key' column
+                    command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
+                    command.Parameters.AddWithValue("@key", key);
+
+                    using (var reader = await command.ExecuteReaderAsync().ConfigureAwait(false))
                     {
-                        columns[i].FromSqlValue(result, reader, converter);
+                        if (!reader.Read()) return null;
+
+                        var result = new T();
+                        for (int i = 1; i < columns.Count; i++) // Skip the first '_Key' column
+                        {
+                            columns[i].FromSqlValue(result, reader, converter);
+                        }
+                        return result;
                     }
-                    return result;
                 }
             }
         }
 
         public async Task<bool> Add(T value)
         {
-            using (var command = connection.CreateCommand())
+            using (var connection = new SqlConnection(connectionString))
             {
-                var sb = StringBuilderCache.Acquire(260);
-                sb.Append("insert into ");
-                sb.Append(tableName);
-                sb.Append(" (");
-
-                for (var i = 0; i < columns.Count; i++)
+                connection.Open();
+                using (var command = connection.CreateCommand())
                 {
-                    sb.Append(columns[i].Name);
-                    if (i != columns.Count - 1) sb.Append(',');
-                }
-                sb.Append(") values(");
+                    var sb = StringBuilderCache.Acquire(260);
+                    sb.Append("insert into ");
+                    sb.Append(tableName);
+                    sb.Append(" (");
 
-                for (var i = 0; i < columns.Count; i++)
-                {
-                    sb.Append("@");
-                    sb.Append(i);
-                    if (i != columns.Count - 1) sb.Append(',');
-                }
-                sb.Append(")");
+                    for (var i = 0; i < columns.Count; i++)
+                    {
+                        sb.Append(columns[i].Name);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
+                    sb.Append(") values(");
 
-                command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
+                    for (var i = 0; i < columns.Count; i++)
+                    {
+                        sb.Append("@");
+                        sb.Append(i);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
+                    sb.Append(")");
 
-                for (int i = 0; i < columns.Count; i++)
-                {
-                    var columnValue = columns[i].ToSqlValue(value, converter);
-                    command.Parameters.AddWithValue("@" + i, columnValue);
-                }
+                    command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
 
-                try
-                {
-                    return await command.ExecuteNonQueryAsync().ConfigureAwait(false) == 1;
-                }
-                catch (SqlException e) when (e.ErrorCode == -2146232060) // 0x80131904
-                {
-                    return false;
+                    for (int i = 0; i < columns.Count; i++)
+                    {
+                        var columnValue = columns[i].ToSqlValue(value, converter);
+                        command.Parameters.AddWithValue("@" + i, columnValue);
+                    }
+
+                    try
+                    {
+                        return await command.ExecuteNonQueryAsync().ConfigureAwait(false) == 1;
+                    }
+                    catch (SqlException e) when (e.ErrorCode == -2146232060) // 0x80131904
+                    {
+                        return false;
+                    }
                 }
             }
         }
 
         public async Task Put(T value)
         {
-            using (var command = connection.CreateCommand())
+            using (var connection = new SqlConnection(connectionString))
             {
-                var sb = StringBuilderCache.Acquire(260);
-                sb.Append("merge ");
-                sb.Append(tableName);
-                sb.Append(" as target using (values (");
-
-                for (var i = 0; i < columns.Count; i++)
+                connection.Open();
+                using (var command = connection.CreateCommand())
                 {
-                    sb.Append("@");
-                    sb.Append(i);
-                    if (i != columns.Count - 1) sb.Append(',');
-                }
+                    var sb = StringBuilderCache.Acquire(260);
+                    sb.Append("merge ");
+                    sb.Append(tableName);
+                    sb.Append(" as target using (values (");
 
-                sb.Append(")) as source (");
+                    for (var i = 0; i < columns.Count; i++)
+                    {
+                        sb.Append("@");
+                        sb.Append(i);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
 
-                for (var i = 0; i < columns.Count; i++)
-                {
-                    sb.Append(columns[i].Name);
-                    if (i != columns.Count - 1) sb.Append(',');
-                }
-                sb.Append(") on target.");
-                sb.Append(SqlColumn.KeyColumnName);
-                sb.Append(" = source.");
-                sb.Append(SqlColumn.KeyColumnName);
+                    sb.Append(")) as source (");
 
-                sb.Append(" when matched then update set ");
-
-                for (var i = 0; i < columns.Count; i++)
-                {
-                    sb.Append(columns[i].Name);
+                    for (var i = 0; i < columns.Count; i++)
+                    {
+                        sb.Append(columns[i].Name);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
+                    sb.Append(") on target.");
+                    sb.Append(SqlColumn.KeyColumnName);
                     sb.Append(" = source.");
-                    sb.Append(columns[i].Name);
-                    if (i != columns.Count - 1) sb.Append(',');
+                    sb.Append(SqlColumn.KeyColumnName);
+
+                    sb.Append(" when matched then update set ");
+
+                    for (var i = 1; i < columns.Count; i++) // Do not update the _Key
+                    {
+                        sb.Append(columns[i].Name);
+                        sb.Append(" = source.");
+                        sb.Append(columns[i].Name);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
+
+                    sb.Append(" when not matched then insert (");
+                    for (var i = 0; i < columns.Count; i++)
+                    {
+                        sb.Append(columns[i].Name);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
+
+                    sb.Append(") values (");
+                    for (var i = 0; i < columns.Count; i++)
+                    {
+                        sb.Append(columns[i].Name);
+                        if (i != columns.Count - 1) sb.Append(',');
+                    }
+
+                    sb.Append(");");
+
+                    command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
+
+                    for (int i = 0; i < columns.Count; i++)
+                    {
+                        var columnValue = columns[i].ToSqlValue(value, converter);
+                        command.Parameters.AddWithValue("@" + i, columnValue);
+                    }
+
+                    await command.ExecuteNonQueryAsync().ConfigureAwait(false);
                 }
-
-                sb.Append(" when not matched then insert (");
-                for (var i = 0; i < columns.Count; i++)
-                {
-                    sb.Append(columns[i].Name);
-                    if (i != columns.Count - 1) sb.Append(',');
-                }
-
-                sb.Append(") values (");
-                for (var i = 0; i < columns.Count; i++)
-                {
-                    sb.Append(columns[i].Name);
-                    if (i != columns.Count - 1) sb.Append(',');
-                }
-
-                sb.Append(");");
-
-                command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
-
-                for (int i = 0; i < columns.Count; i++)
-                {
-                    var columnValue = columns[i].ToSqlValue(value, converter);
-                    command.Parameters.AddWithValue("@" + i, columnValue);
-                }
-
-                await command.ExecuteNonQueryAsync().ConfigureAwait(false);
             }
         }
 
         public async Task<bool> Delete(string key)
         {
-            using (var command = connection.CreateCommand())
+            using (var connection = new SqlConnection(connectionString))
             {
-                var sb = StringBuilderCache.Acquire(260);
-                sb.Append("delete from ");
-                sb.Append(tableName);
-                sb.Append(" where ");
-                sb.Append(SqlColumn.KeyColumnName);
-                sb.Append(" = @key");
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    var sb = StringBuilderCache.Acquire(260);
+                    sb.Append("delete from ");
+                    sb.Append(tableName);
+                    sb.Append(" where ");
+                    sb.Append(SqlColumn.KeyColumnName);
+                    sb.Append(" = @key");
 
-                command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
-                command.Parameters.AddWithValue("@key", key);
+                    command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
+                    command.Parameters.AddWithValue("@key", key);
 
-                return await command.ExecuteNonQueryAsync().ConfigureAwait(false) == 1;
+                    return await command.ExecuteNonQueryAsync().ConfigureAwait(false) == 1;
+                }
             }
         }
 
-        public Task<IEnumerable<T>> Range(string minKey, string maxKey, int? count = default(int?))
+        public async Task<IEnumerable<T>> Range(string minKey, string maxKey, int? count = default(int?))
         {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            var disposable = connection as IDisposable;
-            if (disposable != null)
+            using (var connection = new SqlConnection(connectionString))
             {
-                disposable.Dispose();
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    var hasMin = !string.IsNullOrEmpty(minKey);
+                    var hasMax = !string.IsNullOrEmpty(maxKey);
+
+                    var sb = StringBuilderCache.Acquire(260);
+                    sb.Append("select");
+
+                    if (count != null)
+                    {
+                        sb.Append(" top ");
+                        sb.Append(count.Value);
+                    }
+
+                    sb.Append(" * from ");
+                    sb.Append(tableName);
+
+                    if (hasMin || hasMax)
+                    {
+                        sb.Append(" where ");
+
+                        if (hasMin)
+                        {
+                            sb.Append(SqlColumn.KeyColumnName);
+                            sb.Append(" >= @min");
+                        }
+                        if (hasMax)
+                        {
+                            if (hasMin) sb.Append(" and ");
+                            sb.Append(SqlColumn.KeyColumnName);
+                            sb.Append(" < @max");
+                        }
+                    }
+                    sb.Append(" order by ");
+                    sb.Append(SqlColumn.KeyColumnName);
+
+                    command.CommandText = StringBuilderCache.GetStringAndRelease(sb);
+
+                    if (hasMin) command.Parameters.AddWithValue("@min", minKey);
+                    if (hasMax) command.Parameters.AddWithValue("@max", maxKey);
+
+                    var result = new List<T>();
+                    using (var reader = await command.ExecuteReaderAsync().ConfigureAwait(false))
+                    {
+                        while (reader.Read())
+                        {
+                            var item = new T();
+                            for (int i = 1; i < columns.Count; i++) // Skip the first '_Key' column
+                            {
+                                columns[i].FromSqlValue(item, reader, converter);
+                            }
+                            result.Add(item);
+                        }
+                        return result;
+                    }
+                }
             }
         }
     }
